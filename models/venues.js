5bc386ae682e98ee0d59ef7c12c97e0c68b38e24
geototti21 <geototti21@hotmail.com>
2015-05-17 13:49:27 +0100

    Insert all files

---
+++ models/venues.js
@@ -0,0 +1,35 @@
+//###############################################################
+///// Copyright 2015, Georgoulas Ioannis, All rights reserved.
+/////
+///// Project: Venues recommender API
+/////
+///// File description: (venues.js)
+///// API schema design using mongoose module
+/////
+//###############################################################
+
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var VenueSchema   = new Schema({
+    name: String, // human readable format
+    location: {
+      lat:Number,
+      lon:Number,
+      address: String // in this format -> e.g. 17 Great Junction Street, Edinburgh
+    },
+    price: Number, // scale of 4, 1-2-3-4
+    //money : Number, // Based on Google in the US, 1 is < $10, 2 is $10-$20, 3 is $20-$30, 4 is > $30.
+    distance: Number, // meters from location search
+    rating: Number, //  scale of 10
+    other:{
+      checkins:Number,//only from Foursquare
+      poweredBy: [String], // name of the APIs used
+      timestamp: Number,//timestamp of the request
+      openNow: Boolean
+    }
+
+});
+
+module.exports = mongoose.model('Venue', VenueSchema);
